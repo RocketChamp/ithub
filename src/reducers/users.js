93d959445c1b213ed330a5ca16266e39181efa99
Daniel Ilies <iliesh.daniel@gmail.com>
2022-08-19 16:08:49 +0300

    format chnages

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -3,7 +3,7 @@ import {
   UPDATE_USER,
   DELETE_USER,
   FOLLOW_USER,
-  UNFOLLOW_USER
+  UNFOLLOW_USER,
 } from "../constants/actionTypes.js";
 
 export default (users = [], action) => {
@@ -14,16 +14,19 @@ export default (users = [], action) => {
       return users;
     case UPDATE_USER:
       console.log("UPDATE_USERS PAYLOAD - ", action.payload);
-      const currentUser = JSON.parse(localStorage.getItem('profile'));
+      const currentUser = JSON.parse(localStorage.getItem("profile"));
       const user = { result: action.payload, token: currentUser.token };
-      localStorage.setItem('profile', JSON.stringify(user));
+      localStorage.setItem("profile", JSON.stringify(user));
       users = [...users, user];
-      console.log("users in UPDATE USERS - ", users );
+      console.log("users in UPDATE USERS - ", users);
       return users;
-      case FOLLOW_USER:
-      case UNFOLLOW_USER:
-      const currUser = JSON.parse(localStorage.getItem('profile'));
-      localStorage.setItem('profile', JSON.stringify({result: action.payload, token: currUser.token}));
+    case FOLLOW_USER:
+    case UNFOLLOW_USER:
+      const currUser = JSON.parse(localStorage.getItem("profile"));
+      localStorage.setItem(
+        "profile",
+        JSON.stringify({ result: action.payload, token: currUser.token })
+      );
       return [...users, action.payload];
     case DELETE_USER:
       return users.filter((user) => user._id !== action.payload);
