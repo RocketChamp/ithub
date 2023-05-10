93f6d09aa68876f3e2a66ab387f0c747e113d27e
Daniel Ilies <iliesh.daniel@gmail.com>
2023-05-10 19:01:21 +0300

    removed the console.log()

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const API = axios.create({ baseURL: process.env.REACT_APP_BASE_CUSTOM_URL });
 
-console.log(
-  "process.env.REACT_APP_BASE_CUSTOM_URL",
-  process.env.REACT_APP_BASE_CUSTOM_URL
-);
+// console.log(
+//   "process.env.REACT_APP_BASE_CUSTOM_URL",
+//   process.env.REACT_APP_BASE_CUSTOM_URL
+// );
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
